3be94a760ff87cef2e8e2cc39f8e7ea7468ced52
Neha Das <nehadas2314@gmail.com>
2024-08-06 11:33:43 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Certificate-Verification-System",
+  base: "/Certificate-Verification-System/",
   plugins: [react()],
 });
